0287ad560219a330e44a76d5f878e000772990d9
szarater <szarater@correo.udistrital.edu.co>
2022-10-19 14:50:54 -0500

    setup heroku

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -78,16 +78,28 @@ WSGI_APPLICATION = 'ProyectoWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'libreria',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ADMIN',
+#         'HOST':'127.0.0.1',
+#         'DATABASE_PORT':'5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'libreria',
-        'USER': 'postgres',
-        'PASSWORD': 'ADMIN',
-        'HOST':'127.0.0.1',
+        'NAME': 'de2b5s60dr2b0g',
+        'USER': 'xtwfrqllicnxls',
+        'PASSWORD': '0a8794b25ea2dfb3af5828006d2cf0c0f955979e9ac14bbda7619b78055e3386',
+        'HOST':'ec2-44-210-228-110.compute-1.amazonaws.com',
         'DATABASE_PORT':'5432',
     }
 }
+
 
 # import dj_database_url
 # from decouple import config
