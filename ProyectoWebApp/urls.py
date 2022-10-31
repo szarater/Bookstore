f066cae6142ec54821b853fd179e3d58fb58c858
szarater <szarater@correo.udistrital.edu.co>
2022-10-31 13:46:35 -0500

    The App Tienda is created

--- ProyectoWebApp/urls.py
+++ ProyectoWebApp/urls.py
@@ -5,8 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='Home'),
-    
-    path('tienda', views.tienda, name='Tienda'),
 ]
 static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -7,5 +7,4 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     return render(request, 'ProyectoWebApp/home.html')
 
-def tienda(request):
-    return render(request, 'ProyectoWebApp/tienda.html')
+
